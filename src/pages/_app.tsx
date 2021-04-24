8f942599f20a234e83c53dafa6ddce691ff5c1cc
Gleyson Abreu <gleyson_datu@hotmail.com>
2021-04-24 19:57:43 -0300

    Add meta tag viewport

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { Header } from '../components/Header'; 
 import { Player } from '../components/Player';
 import { PlayerContextProvider } from '../contexts/PlayerContext';
+import Head from 'next/head';
 
 import styles from '../styles/app.module.scss';
 import '../styles/global.scss';
@@ -8,6 +9,9 @@ import '../styles/global.scss';
 function MyApp({ Component, pageProps }) {
   return(
     <PlayerContextProvider>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />          
+      </Head>
       <div className={styles.wrapper}>
         <main>
           <Header />
