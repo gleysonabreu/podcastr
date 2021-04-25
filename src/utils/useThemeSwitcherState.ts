ee7cb9fa1d73aee807dc417e6ad93549a06561f4
Gleyson Abreu <gleyson_datu@hotmail.com>
2021-04-25 11:06:48 -0300

    Fix bug console error

--- src/utils/useThemeSwitcherState.ts
+++ src/utils/useThemeSwitcherState.ts
@@ -7,28 +7,28 @@ type Response<T> = [
 
 function useThemeSwitcherState<T>(key: string, initialize: T): Response<T> {
   const isServer = typeof window === 'undefined';
-
-  let html: HTMLHtmlElement;
-  if (!isServer) {
-    html = window.document.querySelector('html');
-  }
-
   const [state, setState] = useState(() => {
-    if (!isServer) {
+     if (!isServer) {
       const storageValue = localStorage.getItem(key);
 
-      if (storageValue) {
-        return JSON.parse(storageValue);
-      } else {
-        return initialize;
-      }
-    }
+        if (storageValue) {
+          return JSON.parse(storageValue);
+        } else {
+          return initialize;
+        }
+     } else {
+       return initialize;
+     }
   });
 
-  useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(state));
-    html.dataset.theme = state;
-  }, [key, state]);
+  if (!isServer) {
+    const html: HTMLHtmlElement = window.document.querySelector('html');
+
+    useEffect(() => {
+      localStorage.setItem(key, JSON.stringify(state));
+      html.dataset.theme = state;
+    }, [key, state]);
+  }
 
   return [state, setState]
 }
